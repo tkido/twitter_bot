394a3f961b56daeb9e3e88b72159425ee403f8c0
tkido <tkido@xevo.com>
2020-01-07 09:21:41 +0900

    datetime info

--- main.go
+++ main.go
@@ -4,12 +4,45 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"math"
+	"time"
 
 	"github.com/ChimeraCoder/anaconda"
 )
 
+var (
+	api   *anaconda.TwitterApi
+	month int
+	week  int
+	date  int
+)
+
+func init() {
+	// prepare twitter api
+	raw, err := ioutil.ReadFile("config/account/account.json")
+	if err != nil {
+		panic(err)
+	}
+	var acc Account
+	json.Unmarshal(raw, &acc)
+	api = anaconda.NewTwitterApiWithCredentials(acc.AccessToken, acc.AccessTokenSecret, acc.ConsumerKey, acc.ConsumerSecret)
+	// prepare DateTime info
+	now := time.Now()
+	origin := time.Date(1900, 1, 1, 0, 0, 0, 0, time.Local)
+	duration := now.Sub(origin)
+	date = int(math.Ceil(duration.Hours()/24) + 1)
+	week = date / 7
+	month = int(now.Month()) - 1
+}
+
 func main() {
 	fmt.Println("twitter_bot_test")
+	fmt.Println(month)
+	fmt.Println(week)
+	fmt.Println(date)
+
+	fmt.Println(week % 13)
+	fmt.Println(week % 12)
 }
 
 type Account struct {
@@ -20,13 +53,6 @@ type Account struct {
 }
 
 func tweet(text string) error {
-	raw, err := ioutil.ReadFile("config/account/account.json")
-	if err != nil {
-		return err
-	}
-	var acc Account
-	json.Unmarshal(raw, &acc)
-	api := anaconda.NewTwitterApiWithCredentials(acc.AccessToken, acc.AccessTokenSecret, acc.ConsumerKey, acc.ConsumerSecret)
 	tweet, err := api.PostTweet("botのテスト", nil)
 	if err != nil {
 		return err
